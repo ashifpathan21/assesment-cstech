b8817c6b3f0ee213b91f2d96cedacef5e1b63034
Ashif Pathan <ashifbgmiking@gmail.com>
2026-06-02 07:08:20 +0530

    model creation

---
+++ BACKEND/src/models/agent.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+
+const agentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    countryCode: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    assignedTask: [
+        {
+            firstName: {
+                type: String,
+                required: true
+            },
+            phone: {
+                type: String,
+                required: true
+            },
+            notes: {
+                type: String,
+                required: true
+            }
+        }]
+})
+
+export default mongoose.model('agent', agentSchema);
