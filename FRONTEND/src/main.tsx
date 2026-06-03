563fed4ad2e75ed49322368f7790b2c07d4c6f9f
Ashif Pathan <ashifbgmiking@gmail.com>
2026-06-03 14:11:27 +0530

    google

--- FRONTEND/src/main.tsx
+++ FRONTEND/src/main.tsx
@@ -3,9 +3,15 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-    <Toaster />
-  </BrowserRouter>,
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
+  </GoogleOAuthProvider>,
 );
